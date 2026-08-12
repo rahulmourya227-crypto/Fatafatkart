d6d08b41cbea0d2b1292757567ba3f0c28462293
rahulmourya227-crypto <rahulmourya227@gmail.com>
2026-08-12 22:46:13 +0530

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import FatafatKart from "./App.jsx";
+import AdminDashboard from "./AdminDashboard.jsx";
 import "./index.css";
+
+const isAdmin = window.location.pathname.startsWith("/admin");
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <FatafatKart />
+    {isAdmin ? <AdminDashboard /> : <FatafatKart />}
   </React.StrictMode>
 );
